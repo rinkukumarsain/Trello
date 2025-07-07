86fe243db03ea6889e9b088e327ac4d67ea226ea
Sharthak17051 <sharthak.img@gmail.com>
2025-07-07 12:17:26 +0530

    Setup

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`);
+})
